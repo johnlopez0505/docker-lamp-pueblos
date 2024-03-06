a8e8cc3df4ba2fe48a77d17ff2da0a9ce8020191
johnlopez0505 <lopezcon1@hotmail.com>
2024-03-06 09:40:07 +0100

    06-03-2024

--- dump/restaurantDb.sql
+++ dump/restaurantDb.sql
@@ -23,12 +23,9 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `log`
---
 
 CREATE TABLE `log` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) AUTO_INCREMENT PRIMARY KEY,
   `log` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -45,8 +42,20 @@ INSERT INTO `log` (`log`) VALUES
 -- Estructura de tabla para la tabla `restaurantes`
 --
 
+CREATE TABLE `usuarios` (
+  `id` int(4) PRIMARY KEY AUTO_INCREMENT,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(240) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `imagen` varchar(200) DEFAULT NULL,
+  `disponible` tinyint(1) NOT NULL,
+  `token` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='tabla de usuarios';
+
+
+
 CREATE TABLE `restaurantes` (
-  `id` int(7) NOT NULL AUTO_INCREMENT,
+  `id` int(7) AUTO_INCREMENT PRIMARY KEY,
   `id_usuario` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `ciudad` varchar(100) NOT NULL,
@@ -55,82 +64,21 @@ CREATE TABLE `restaurantes` (
   `imagen` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `restaurantes`
---
+ALTER TABLE `restaurantes`
+   ADD CONSTRAINT FK_id_usuario FOREIGN KEY (id_usuario)
+      REFERENCES `usuarios` (id);
 
+
+INSERT INTO `usuarios` (`email`, `password`, `nombre`, `imagen`, `disponible`, `token`) VALUES
+('lopezcon1@hotmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'john lopez contreras', null,1,null),
+('dario@hotmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'dario romero romera', null,1,null), 
+('francisco@hotmail.com','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','francisco nolasco romero', NULL, 1, null),
+('srodher115@g.educaand.es', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'santiago', NULL, 1, null);
+
+
+ 
 INSERT INTO `restaurantes` (`id_usuario`, `nombre`, `ciudad`,`provincia`,`telefono`, `imagen`) VALUES
 (1, 'Casa marcial', 'Arriondas','Asturias','985 84 09 91',null)
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(4) NOT NULL COMMENT 'clave principal',
-  `email` varchar(150) NOT NULL,
-  `password` varchar(240) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `imagen` varchar(200) DEFAULT NULL,
-  `disponible` tinyint(1) NOT NULL,
-  `token` varchar(240) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='tabla de usuarios';
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `email`, `password`, `nombre`, `imagen`, `disponible`, `token`) VALUES
-(1, 'lopezcon1@hotmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'john lopez contreras', NULL, 1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NjIxMzczNzIsImRhdGEiOnsiaWQiOiIxIiwiZW1haWwiOiJkYXZpZHJvZEBnbWFpbC5jb20ifX0.FLlqJO30GgMiYWFNSXFjIWunenCjb7EnZJ30PSJdAN8'),
-(2, 'dario@hotmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'dario romero romera', NULL, 1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzM3NzU0MDEsImRhdGEiOnsiaWQiOiIyIiwiZW1haWwiOiJzb25pYW1lbmFkZWxAZ21haWwuY29tIn19.33d9tDvm1jRJ-fzdz1-leoRQ5EMnnrxuY7BNDqatl5g'),
-(2, 'francisco@hotmail.com', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'francisco nolasco romero', NULL, 1, null),
-(3, 'srodher115@g.educaand.es', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'santiago', NULL, 1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NzM3NzU2ODYsImRhdGEiOnsiaWQiOiIzIiwiZW1haWwiOiJzcm9kaGVyMTE1QGcuZWR1Y2FhbmQuZXMifX0.9Wb7_IMl_pLDzcPY8IH1SU4XUrOY5sdtC1Vhxr7_44c');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `log`
---
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `restaurantes`
---
-ALTER TABLE `restaurantes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `log`
---
--- ALTER TABLE `log`
---   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `restaurantes`
---
--- ALTER TABLE `restaurantes`
---   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
--- ALTER TABLE `usuarios`
---   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'clave principal', AUTO_INCREMENT=4;
--- COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
